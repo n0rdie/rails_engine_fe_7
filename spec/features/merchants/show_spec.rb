d4e11f82f814b33991e8ec112b245a7994dd6467
Quin Nordmark <quinnordmark@gmail.com>
2024-02-15 10:43:57 -0700

    Merchant Show done

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Merchants Index' do
     before(:each) do
-        merchant_1 = Merchant.create(name: "Steve")
-        merchant_1.items.create(name: "Dirt")
-        merchant_1.items.create(name: "Cobblestone")
-        merchant_1.items.create(name: "Diamond")
+        @merchant_1 = Merchant.create(name: "Steve")
+        @merchant_1.items.create(name: "Dirt")
+        @merchant_1.items.create(name: "Cobblestone")
+        @merchant_1.items.create(name: "Diamond")
     end 
 
     it "Merchants Index" do
@@ -13,7 +13,7 @@ RSpec.describe 'Merchants Index' do
         visit '/merchants'
         # and when I click the merchant's name
         click_on 'Steve'
-        expect(current_path).to eq("/merchants/#{merchant_1.id}")
+        expect(current_path).to eq("/merchants/#{@merchant_1.id}")
         # And I should see a list of items that merchant sells.
         expect(page).to have_content('Dirt')
         expect(page).to have_content('Cobblestone')
