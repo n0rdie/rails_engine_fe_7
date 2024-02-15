732bee99dc0c9d780005b7bf7fd20c6c855bb693
Quin Nordmark <quinnordmark@gmail.com>
2024-02-15 11:14:19 -0700

    Reorganize specs

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchants Index' do
-    before(:each) do
+    it "Merchants Index" do
         merchant_1 = Merchant.create(name: "Steve")
         merchant_2 = Merchant.create(name: "Bob")
         merchant_3 = Merchant.create(name: "Jeff")
         merchant_4 = Merchant.create(name: "Alex")
-    end 
 
-    it "Merchants Index" do
         # When I visit '/merchants'
         visit '/merchants'
         # I should see a list of merchants by name
--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -1,19 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchants Index' do
-    before(:each) do
-        @merchant_1 = Merchant.create(name: "Steve")
-        @merchant_1.items.create(name: "Dirt")
-        @merchant_1.items.create(name: "Cobblestone")
-        @merchant_1.items.create(name: "Diamond")
-    end 
-
     it "Merchants Index" do
+        merchant_1 = Merchant.create(name: "Steve")
+        merchant_1.items.create(name: "Dirt")
+        merchant_1.items.create(name: "Cobblestone")
+        merchant_1.items.create(name: "Diamond")
+
         # When I visit '/merchants'
         visit '/merchants'
         # and when I click the merchant's name
         click_on 'Steve'
-        expect(current_path).to eq("/merchants/#{@merchant_1.id}")
+        expect(current_path).to eq("/merchants/#{merchant_1.id}")
         # And I should see a list of items that merchant sells.
         expect(page).to have_content('Dirt')
         expect(page).to have_content('Cobblestone')
